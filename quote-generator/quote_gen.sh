6e1eb9d563f20f65880fc15f1d75874a2af85faa
Becky Shaw <reshaw93@gmail.com>
2024-10-28 10:45:06 +0000

    [quoteGen---Update] Updated random element line:10 Amended to add signed commit Amend num.2

--- quote-generator/quote_gen.sh
+++ quote-generator/quote_gen.sh
@@ -1,16 +1,27 @@
+# #!/bin/bash
+
+# # Path to your quotes file
+# QUOTES_FILE="$HOME/Git/Andromeda/quote-generator/quotes.txt"
+
+# # Count the number of lines in the quotes file
+# NUM_QUOTES=$(wc -l < "$QUOTES_FILE")
+
+# # Generate a random number within the range of number of quotes
+# RANDOM_NUM=$(( RANDOM % NUM_QUOTES + 1 ))
+
+# # Get the quote corresponding to the random line number
+# QUOTE=$(sed -n "${RANDOM_NUM}p" "$QUOTES_FILE")
+
+# # Display the quote
+# echo "$QUOTE"
+
 #!/bin/bash
 
 # Path to your quotes file
-QUOTES_FILE="$HOME/git/andromeda/quote-generator/quotes.txt"
+QUOTES_FILE="$HOME/Git/Andromeda/quote-generator/quotes.txt"
 
-# Count the number of lines in the quotes file
-NUM_QUOTES=$(wc -l < "$QUOTES_FILE")
-
-# Generate a random number within the range of number of quotes
-RANDOM_NUM=$(( RANDOM % NUM_QUOTES + 1 ))
-
-# Get the quote corresponding to the random line number
-QUOTE=$(sed -n "${RANDOM_NUM}p" "$QUOTES_FILE")
+# Select a random quote using shuf
+QUOTE=$(shuf -n 1 "$QUOTES_FILE")
 
 # Display the quote
 echo "$QUOTE"
