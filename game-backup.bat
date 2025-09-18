29438f2445258e840e6281248079f3f97349ac33
Beck Shaw <reshaw93@gmail.com>
2025-09-18 22:30:54 +0100

    [Gnome] Added instructions in game-backup.bat

--- game-backup.bat
+++ game-backup.bat
@@ -4,3 +4,8 @@ xcopy /s /i "{source_folder}" "{destination_folder}\meabackup_%date:~-4,4%%date:
 echo x=msgbox("{Game Title} game save files backed up successfully!",0,"Backup Game Save")>>msgbox.vbs
 start msgbox.vbs
 
+# source_folder and destination_folder should be replaced with actual paths before running the script.
+# game title should also be replaced with the actual game title to appear in the message box.
+#
+# Optional: You can add this script to Windows Task Scheduler to run at specific intervals.
+# Optional: save this file with a .bat extension, e.g., game-backup.bat and copy to desktop next to game boot shortcut for easy access.
